5534df7dc6c0f6f2f70e0930395f1c567e34e6a5
Ruslan <ruslokoboy@mail.ru>
2020-03-11 22:11:55 +0300

    task1

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-
-char* startptr = nullptr;
-char* curptr = nullptr;
-size_t alloc_size = 0;
-size_t cursize = 0;
+#include "task1.h"
 
 void MakeAllocator(size_t maxsize) {
     startptr = (char*) malloc(maxsize);
--- task1/task1.h
+++ task1/task1.h
@@ -1,9 +1,10 @@
+#pragma once
 #include <iostream>
 
-void* startptr;
-void* curptr;
-size_t alloc_size;
-size_t cursize;
+char* startptr = nullptr;
+char* curptr = nullptr;
+size_t alloc_size = 0;
+size_t cursize = 0;
 void MakeAllocator(size_t maxsize);
 char* Alloc(size_t size);
 void Reset();
