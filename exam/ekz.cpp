848d742d536e41a9b1280ebc0931a6e7bfbd5d86
Your Name <ruslokoboy@mail.ru>
2020-06-11 09:48:00 -0700

    fix

--- exam/ekz.cpp
+++ exam/ekz.cpp
@@ -9,23 +9,29 @@
 #include <sstream>
 
 std::ifstream file;
-std::queue<int> queue;
+std::queue<std::string> queue;
 bool read = false;
 unsigned int sum = 0;
 std::mutex mutex_q;
 std::mutex mutex_s;
 
 void sumator(){
+    std::string str;
     unsigned int num;
     while(!read || !queue.empty()) {
+        
         mutex_q.lock();
         if(queue.empty()) {
             mutex_q.unlock();
             continue;
         }
-        num = queue.front();
+        str = queue.front();
         queue.pop();
         mutex_q.unlock();
+        
+        std::stringstream ss(str);
+        num = 0;
+        for(int number; ss >> number; num += number);
         mutex_s.lock();
         sum += num;
         mutex_s.unlock();
@@ -34,12 +40,12 @@ void sumator(){
 }
 
 void read_file(){
-    unsigned int num;
     while (!file.eof()){
-        file >> num;
-        if(!file.eof()) {
+        std::string str;
+        std::getline(file, str);
+        if (!str.empty()){
             mutex_q.lock();
-            queue.push(num);
+            queue.push(str);
             mutex_q.unlock();
         }
     }
@@ -48,7 +54,7 @@ void read_file(){
 
 int main(){
     std::string name;
-    std::cin >> name;
+    name = "test.txt";
     file.open(name);
     std::thread thread(read_file);
     std::thread thread_1(sumator);
